0d97b17932f183b724bc1a0eaf7be1a230e85edd
Sammy Akharaz <ketrixz@gmail.com>
2018-05-21 03:39:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,13 +3,20 @@ class Artist
   attr_writer name
   attr_reader
   
+  @@all = []
+  
   def initialize(name)
     @name = name
+    
+    @all << self
   end
   
   def name
     @name
+  end
   
+  def self.all
+    @@all
   end
   
 end
